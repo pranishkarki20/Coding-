67830603f14a374e178066cb0529c99a632c8de6
Pranish Karki <praniskkarki20@gmail.com>
2026-02-07 14:52:01 +0530

    js

---
+++ math.js
@@ -0,0 +1,21 @@
+//math is a bulit in object that provides a collection and methods
+
+let x = -3.5 ;
+let y = 2 ;
+let a = 45 ;
+let z ; 
+
+//z = Math.round(x); // roundup 
+//z = Math.floor(x); //rounddown
+//z=Math.ceil(x); //opposite of floor
+//z = Math.trunc(x);   //removes the  decimal point 
+//z= Math.pow(x, y) ;
+//z=Math.sqrt(x);
+//z= Math.sin(a)
+//z=Math.abs(x);
+//z=Math.sign(x);
+
+let max = Math.max(x,y,a);
+let min = Math.min(x,y,a);
+console.log(max);
+console.log(min);
