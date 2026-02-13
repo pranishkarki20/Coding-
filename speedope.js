cf7d1a4df4934e805c397c89f607f699a70cbad5
pranishkarki20 <praniskkarki20@gmail.com>
2026-02-13 23:38:36 +0530

    spped operator

---
+++ speedope.js
@@ -0,0 +1,9 @@
+/*let numbers = [1,2,3,4,5];
+let maximum = Math.max(...numbers);
+console.log(numbers);
+console.log(maximum)*/
+
+const a = [1,3];
+const b = [1,5];
+const result = [...a, ...b];
+console.log(result)
